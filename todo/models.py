05add22d84e882b51fcdccfec9d3bbec084a1b33
Stuartpkd <stuartparkerdesign@gmail.com>
2023-06-27 14:13:27 +0000

    Add item model list for user account

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+
+    name = models.CharField(max_length=50, null=False, black=False)
+    done = models.BooleanField(null=False, black=False, default=False)
