bf3c0bc523314ef3853868c368680b26893c18e9
Stuartpkd <stuartparkerdesign@gmail.com>
2023-06-27 14:28:08 +0000

    Add code for naming items in models admin page

--- todo/models.py
+++ todo/models.py
@@ -5,5 +5,8 @@ from django.db import models
 
 class Item(models.Model):
 
-    name = models.CharField(max_length=50, null=False, black=False)
-    done = models.BooleanField(null=False, black=False, default=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
